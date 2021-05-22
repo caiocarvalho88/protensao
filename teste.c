d2b2cc860638036f5ddbe6c6349713e6a008793c
Caio Carvalho <caiocarvalho88@gmail.com>
2021-05-22 18:24:44 -0300

    organização do codigo para melhor entendimento

--- teste.c
+++ teste.c
@@ -3,12 +3,18 @@
 #include <conio.h>
 #include <math.h>
 #include <tgmath.h>
+/******* Área dos protótipos ******/
+void resultado(int i, float *comp, float *D, float *ang, float *da, float *dar, float *e, float *p);
+/******* fim dos protótipos *******/
 
+
+/******** FUNÇÃO PRINCIPAL *******/
 int main()
 {
-   int x;
-   float fkn, aco, f, tensao, beta, mi;
-   int s, n;
+    int x, s, n, i;
+    float fkn, aco, f, tensao, beta, mi;
+    float *comp, *ang, *vetorz, *D, *da, *dar, *e, *p;
+    const double PI  = 3.141592653589793238463;
 
     // Etapa 01 - Definição de grandeza física a ser calculada
     printf("digite a grandeza fisica a ser utilizada:\n 1- Tensao MPa \n 2-Forca\n (kN)\n");
@@ -50,23 +56,17 @@ int main()
         scanf("%d%f%f",&s,&beta,&mi);
          printf("%d   %f   %f\n", s, beta, mi);
         n = s+1;
-        float *comp, *ang, *vetorz;
-    
-        comp = (float *) malloc(n * sizeof(float));
-        ang = (float *) malloc(n * sizeof(float));
-        vetorz = (float *) malloc(n * sizeof(float));
+        //vetores de entrada de dados 
+        comp = (float *) malloc(n * sizeof(float));     //comprimento da seção analisada
+        ang = (float *) malloc(n * sizeof(float));      //ângulo alfa em graus
+        vetorz = (float *) malloc(n * sizeof(float));   //????
 
-        //variáveis auxiliares
-        float *D, *da, *dar, *a, *p, *e;
-        D = (float *) malloc(n * sizeof(float));
-        da = (float *) malloc(n * sizeof(float));
-        dar = (float *) malloc(n * sizeof(float));
-        a = (float *) malloc(n * sizeof(float));
-        p = (float *) malloc(n * sizeof(float));
-        e = (float *) malloc(n * sizeof(float));
-        const double PI  = 3.141592653589793238463;
-        float b;
-        int i;
+        //vetores auxiliares
+        D = (float *) malloc(n * sizeof(float));        //comprimento acumulado
+        da = (float *) malloc(n * sizeof(float));       //alfa acumulado em graus
+        dar = (float *) malloc(n * sizeof(float));      //alfa acumulado em radiano
+        e = (float *) malloc(n * sizeof(float));        //fator redutor de tensão por perda de tensão
+        p = (float *) malloc(n * sizeof(float));        //tensão resultante após a perda de tensão
     
         //inicialização dos vetores de operação
         D[0]=0;
@@ -101,16 +101,12 @@ int main()
                         e[i] = exp(-mi * (dar[i]+beta*(D[i]))); //fator redutor de tensão por perda de tensão
                         p[i] = f*e[i]; // tensão resultante após a perda de tensão
                 }
-                printf("secao \t comp. \t acum. \t alfa \t ang. acum. \t fator \t tens. resul. \n");
-                for(i=0;i<n;i++)
-                {
-                    printf("%d \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f \n", i, comp[i], D[i], ang[i], da[i], dar[i], e[i], p[i]);
-                }
+                resultado(n, comp, D, ang, da, dar, e, p);
                 printf("a protensao eh feita em ambos os lados?\n digite:\n 1- para SIM\n 2- para NAO");
                 scanf("%d",&x);
                 while(x!=1&x!=2)
                 {
-                    printf("Esta escolha nao existe \n \n a protensao eh feita em ambos os lados?\n digite:\n 1- para SIM\n 2- para NAO");
+                    printf("Esta escolha nao existe \n \n a protensao eh feita em ambos os lados?\n digite:\n 1- para SIM\n 2- para NAO\n");
                     scanf("%d",&x);
                 }
                 if(x==1)
@@ -126,4 +122,21 @@ int main()
     printf("fim\n");
     return 0;
 }
-
+/********* FIM DA FUNÇÃO PRINCIPAL *******/
+/*--------- Corpo das funções --------- */
+/*
+***** Função resultado *****
+Parâmetros de entrada: int n e os floats *comp, *D, *ang, *da, *dar, *e, *p
+Objetivo: imprimir a tabela de resultados de perda de protenção na tela
+Tipo de retorno: não tem (void)
+*/
+void resultado(int n, float *comp, float *D, float *ang, float *da, float *dar, float *e, float *p)
+{
+    int i;
+    printf("secao \t comp. \t acum. \t alfa \t ang. acum. \t fator \t tens. resul. \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f \t %.2f \n", i, comp[i], D[i], ang[i], da[i], dar[i], e[i], p[i]);
+    }
+  return; /* retorno de uma função void  */
+}
